9aaf4606fd763313c6e7ed27ebddcda425546c00
Jack Pettit <skelatorjack@gmail.com>
2021-09-06 23:10:32 -0500

    Copied backend.

---
+++ backend/jobapplicationtrackerbackend/src/main/java/com/jpettit/jobapplicationtrackerbackend/JobapplicationtrackerbackendApplication.java
@@ -0,0 +1,13 @@
+package com.jpettit.jobapplicationtrackerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobapplicationtrackerbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobapplicationtrackerbackendApplication.class, args);
+	}
+
+}
---
+++ backend/jobapplicationtrackerbackend/src/test/java/com/jpettit/jobapplicationtrackerbackend/JobapplicationtrackerbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpettit.jobapplicationtrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobapplicationtrackerbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
