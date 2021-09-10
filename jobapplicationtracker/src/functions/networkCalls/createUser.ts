2bf3f899fef8632da88948fc60b7be53a6dd06d0
Jack Pettit <skelatorjack@gmail.com>
2021-09-10 14:59:35 -0500

    JA-4 User Sign Up
    The function createUser now returns an HttpResponse of type String. This returns more information from createUser.

--- jobapplicationtracker/src/functions/networkCalls/createUser.ts
+++ jobapplicationtracker/src/functions/networkCalls/createUser.ts
@@ -2,14 +2,16 @@ import axios from "axios";
 import { error } from "console";
 import URLPath from "../../enums/URLPath_enum";
 import User from "../../models/User";
+import HttpResponse from "../../models/HttpResponse";
 
-async function createUser(user: User): Promise<string> {
+async function createUser(user: User): Promise<HttpResponse<string>> {
     const result = await axios.post(URLPath.createUser, user);
+    const dataAsString: string = result.data;
 
     if (result.status >= 400) {
-        return `result.data`;
+        return new HttpResponse<string>(result.status, dataAsString, "");
     } else {
-        return "";
+        return new HttpResponse<string>(result.status, "", dataAsString);
     }
 }
 
