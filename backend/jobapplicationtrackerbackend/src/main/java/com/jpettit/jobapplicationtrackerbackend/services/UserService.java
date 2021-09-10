72a0fff63d43e57e6dfa6396ff62d42edb8eccf3
Jack Pettit <skelatorjack@gmail.com>
2021-09-10 14:57:19 -0500

    JA-4 User Sign Up
    Added DAO, DatabaseService, and UserService.

---
+++ backend/jobapplicationtrackerbackend/src/main/java/com/jpettit/jobapplicationtrackerbackend/services/UserService.java
@@ -0,0 +1,10 @@
+package com.jpettit.jobapplicationtrackerbackend.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public UserService() {
+        System.out.println("In UserService Constructor");
+    }
+}
