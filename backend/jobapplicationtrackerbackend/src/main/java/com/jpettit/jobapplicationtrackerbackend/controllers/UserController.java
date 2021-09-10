91fac55be0dd0de51c5a05d438a77d50f4fddfb0
Jack Pettit <skelatorjack@gmail.com>
2021-09-10 14:58:13 -0500

    JA-4 User Sign Up
    Added UserController.

--- backend/jobapplicationtrackerbackend/src/main/java/com/jpettit/jobapplicationtrackerbackend/controllers/UserController.java
+++ backend/jobapplicationtrackerbackend/src/main/java/com/jpettit/jobapplicationtrackerbackend/controllers/UserController.java
@@ -2,17 +2,37 @@ package com.jpettit.jobapplicationtrackerbackend.controllers;
 
 import com.jpettit.jobapplicationtrackerbackend.helpers.UserControllerURL;
 import com.jpettit.jobapplicationtrackerbackend.models.User;
+import com.jpettit.jobapplicationtrackerbackend.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.PostConstruct;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestController
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println("In post construct at time ");
+    }
 
     @PostMapping(UserControllerURL.addUser)
     public ResponseEntity<String> addUser(@RequestBody User newUser) {
-        System.out.println("Added user.");
+        System.out.println("Added user at time .");
 
         return new ResponseEntity<>("", HttpStatus.CREATED);
     }
+
+//    private String getTime() {
+//        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+//        LocalDateTime now = LocalDateTime.now();
+//
+//        return dtf.format(now);
+//    }
 }
---
+++ backend/jobapplicationtrackerbackend/src/test/java/com/jpettit/jobapplicationtrackerbackend/controllers/UserControllerTest.java
@@ -0,0 +1,13 @@
+package com.jpettit.jobapplicationtrackerbackend.controllers;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(controllers = UserController.class)
+class UserControllerTest {
+
+}
