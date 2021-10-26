a8446e4f482d31a558ad18ccb5764b58c99d7ad5
Jack Pettit <skelatorjack@gmail.com>
2021-10-26 00:35:56 -0500

    JA-5 Login
    Removed unused imports.

--- backend/jobapplicationtrackerbackend/src/test/java/helpers/UserServiceHelper.java
+++ backend/jobapplicationtrackerbackend/src/test/java/helpers/UserServiceHelper.java
@@ -1,9 +1,6 @@
 package helpers;
 
-import com.jpettit.jobapplicationtrackerbackend.models.Session;
-import com.jpettit.jobapplicationtrackerbackend.models.User;
-import com.jpettit.jobapplicationtrackerbackend.models.UserServiceIntPair;
-import com.jpettit.jobapplicationtrackerbackend.models.UserServiceUserPair;
+import com.jpettit.jobapplicationtrackerbackend.models.*;
 
 import java.time.LocalDate;
 import java.util.Optional;
@@ -16,6 +13,8 @@ public class UserServiceHelper {
     public static final User user2 = User.createUser("u2", "e2", "p2",
             Session.createSession("goodSession", LocalDate.of(2000, 1, 1)), 1L);
     public static final Optional<User> nullUser = Optional.empty();
+    public static final Login login = Login.createLogin("u1", "p1");
+    public static final Login nonExistantLogin = Login.createLogin("dude", "p5");
 
     public static void verifyUserServiceUserPair(UserServiceIntPair actual, UserServiceIntPair expected) {
         TestPair<Integer> countPair = new TestPair<>(actual.getValue(), expected.getValue());
