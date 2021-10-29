7f76be20daff3daa18454c4f4cd3888309065f4b
Jack Pettit <skelatorjack@gmail.com>
2021-10-29 18:37:30 -0500

    Added test class for UserQuerier.

---
+++ backend/jobapplicationtrackerbackend/src/test/java/com/jpettit/jobapplicationtrackerbackend/helpers/UserQuerierTest.java
@@ -0,0 +1,12 @@
+package com.jpettit.jobapplicationtrackerbackend.helpers;
+
+import org.junit.jupiter.api.AfterEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserQuerierTest {
+
+    @AfterEach
+    void tearDown() {
+    }
+}
