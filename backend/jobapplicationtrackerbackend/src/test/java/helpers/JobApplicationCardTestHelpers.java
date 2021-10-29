3b2550c6e9d3825ce3e62b0990d456f5910b714c
Jack Pettit <skelatorjack@gmail.com>
2021-10-29 18:32:46 -0500

    Added JobApplicationCardTestHelpers.

---
+++ backend/jobapplicationtrackerbackend/src/test/java/helpers/JobApplicationCardTestHelpers.java
@@ -0,0 +1,64 @@
+package helpers;
+
+import com.jpettit.jobapplicationtrackerbackend.models.Interview;
+import com.jpettit.jobapplicationtrackerbackend.models.JobApplication;
+import com.jpettit.jobapplicationtrackerbackend.models.JobApplicationCard;
+import org.junit.jupiter.api.Assertions;
+
+import java.sql.Date;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class JobApplicationCardTestHelpers {
+    private static final String TITLE_KEY = "title";
+    private static final String COMPANY_KEY = "company";
+    private static final String COUNT_KEY = "count";
+    private static final String ID_KEY = "id";
+
+    public static JobApplication createDefaultJobApp() {
+        final ArrayList<Interview> INTERVIEWS = createDefaultInterviews();
+
+        return JobApplication.createJobApplication("iOS Developer", "EJ", "1",
+                LocalDate.now(), "user1", "Pending", INTERVIEWS);
+    }
+
+    public static ArrayList<Interview> createDefaultInterviews() {
+        final Interview[] INTERVIEWS = {
+                Interview.createInterview("1", "1", "Phone interview",
+                        Date.valueOf(LocalDate.now()), "Next Round"),
+                Interview.createInterview("2", "1", "Phone interview",
+                        Date.valueOf(LocalDate.now()), "Next Round"),
+                Interview.createInterview("3", "1", "Phone interview",
+                        Date.valueOf(LocalDate.now()), "Next Round")
+        };
+
+        return new ArrayList<>(Arrays.asList(INTERVIEWS));
+    }
+
+    public static void verifyIfJobAppCardAndJobAppAreEqual(JobApplicationCard card, JobApplication jobApp) {
+        HashMap<String, String> map = getErrorMessages(card, jobApp);
+
+        Assertions.assertEquals(jobApp.getJobAppTitle(), card.getJobTitle(),
+                map.getOrDefault(TITLE_KEY, ""));
+        Assertions.assertEquals(jobApp.getCompanyName(), card.getCompanyName(),
+                map.getOrDefault(COMPANY_KEY, ""));
+        Assertions.assertEquals(jobApp.getInterviews().size(), card.getInterviewCount(),
+                map.getOrDefault(COUNT_KEY, ""));
+        Assertions.assertEquals(jobApp.getId(), card.getJobAppId(),
+                map.getOrDefault(ID_KEY, ""));
+    }
+
+    public static HashMap<String, String> getErrorMessages(JobApplicationCard card, JobApplication app) {
+       HashMap<String, String> map = new HashMap<>();
+
+       map.put("title", String.format("Expected %s, got %s instead.", card.getJobTitle(), app.getJobAppTitle()));
+       map.put("company", String.format("Expected %s, got %s instead.", card.getCompanyName(), app.getCompanyName()));
+       map.put("count", String.format("Expected %s, got %s instead.", card.getInterviewCount(),
+               app.getInterviews().size()));
+       map.put("id", String.format("Expected %s, got %s instead.", card.getJobAppId(), app.getId()));
+
+       return map;
+    }
+}
