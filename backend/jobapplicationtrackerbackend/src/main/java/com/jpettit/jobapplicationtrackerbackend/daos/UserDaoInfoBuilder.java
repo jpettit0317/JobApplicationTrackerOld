c1d5164e71a0adc218b2ea9084fe12c17e4be3ba
Jack Pettit <skelatorjack@gmail.com>
2021-11-02 23:06:04 -0500

    Added Component and constructor.

--- backend/jobapplicationtrackerbackend/src/main/java/com/jpettit/jobapplicationtrackerbackend/daos/UserDaoInfoBuilder.java
+++ backend/jobapplicationtrackerbackend/src/main/java/com/jpettit/jobapplicationtrackerbackend/daos/UserDaoInfoBuilder.java
@@ -1,4 +1,24 @@
 package com.jpettit.jobapplicationtrackerbackend.daos;
 
+import com.jpettit.jobapplicationtrackerbackend.database.JobAppTrackerConnection;
+import com.jpettit.jobapplicationtrackerbackend.enums.AppProperties;
+import com.jpettit.jobapplicationtrackerbackend.helpers.ProjectEnvironment;
+import com.jpettit.jobapplicationtrackerbackend.helpers.ProjectEnvironmentReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Connection;
+
+@Component
 public class UserDaoInfoBuilder {
+    private final Connection connection;
+    private final ProjectEnvironment environment;
+
+    public UserDaoInfoBuilder(Connection conn,
+                              @Value(AppProperties.appEnv) String env) {
+        connection = JobAppTrackerConnection.createConnection().get();
+        environment = ProjectEnvironmentReader.getEnvironment(env);
+    }
+
+
 }
