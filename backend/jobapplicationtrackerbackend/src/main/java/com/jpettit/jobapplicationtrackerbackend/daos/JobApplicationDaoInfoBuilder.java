c60393727cfdc52b504bd9f2e96d2e1d83a76f62
Jack Pettit <skelatorjack@gmail.com>
2021-11-04 22:23:19 -0500

    Added Component annotation and constructor.

--- backend/jobapplicationtrackerbackend/src/main/java/com/jpettit/jobapplicationtrackerbackend/daos/JobApplicationDaoInfoBuilder.java
+++ backend/jobapplicationtrackerbackend/src/main/java/com/jpettit/jobapplicationtrackerbackend/daos/JobApplicationDaoInfoBuilder.java
@@ -1,4 +1,17 @@
 package com.jpettit.jobapplicationtrackerbackend.daos;
 
+import com.jpettit.jobapplicationtrackerbackend.enums.AppProperties;
+import com.jpettit.jobapplicationtrackerbackend.helpers.ProjectEnvironment;
+import com.jpettit.jobapplicationtrackerbackend.helpers.ProjectEnvironmentReader;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class JobApplicationDaoInfoBuilder {
+    private final ProjectEnvironment environment;
+
+    public JobApplicationDaoInfoBuilder(@Value(AppProperties.appEnv) String env) {
+        environment = ProjectEnvironmentReader.getEnvironment(env);
+    }
+
 }
