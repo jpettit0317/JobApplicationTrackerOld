1105ffc727d56d0864f347ea1358dd1fcdd41607
Jack Pettit <skelatorjack@gmail.com>
2021-11-05 20:13:34 -0500

    Added a test for the method getJobAppCards. This test tests the valid path.

--- backend/jobapplicationtrackerbackend/src/test/java/com/jpettit/jobapplicationtrackerbackend/daos/JobApplicationDAOTest.java
+++ backend/jobapplicationtrackerbackend/src/test/java/com/jpettit/jobapplicationtrackerbackend/daos/JobApplicationDAOTest.java
@@ -3,18 +3,24 @@ package com.jpettit.jobapplicationtrackerbackend.daos;
 import com.jpettit.jobapplicationtrackerbackend.models.JobApplicationCard;
 import com.jpettit.jobapplicationtrackerbackend.models.ResultPair;
 import helpers.JobApplicationDAOTestHelper;
+import helpers.JobApplicationDAOTestHelperVars;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 @TestPropertySource(properties = {
         "application.env=test"
 })
@@ -36,5 +42,26 @@ class JobApplicationDAOTest {
         final JobApplicationDAOTestHelper<ResultPair<ArrayList<JobApplicationCard>>> HELPER = new JobApplicationDAOTestHelper<>(ACTUAL_PAIR, EXPECTED_PAIR);
 
         HELPER.verifyResultPairOfJobAppCardsAreEqual();
+    }
+
+    @Test
+    public void testGetJobAppCards_whenPassedInUser1_ShouldReturnJobApplicationCardsAndNoErrorMessage() throws SQLException {
+        final String USERNAME = JobApplicationDAOTestHelperVars.USER1_NAME;
+        final ArrayList<JobApplicationCard> CARDS = JobApplicationDAOTestHelperVars.createDefaultCards();
+        final ResultPair<ArrayList<JobApplicationCard>> EXPECTED = new ResultPair<>(CARDS, "");
+
+        Mockito.when(builder.getJobAppCards(USERNAME))
+                .thenReturn(EXPECTED);
+
+        final ResultPair<ArrayList<JobApplicationCard>> ACTUAL = sut.getJobAppCards(USERNAME);
+
+        final JobApplicationDAOTestHelper<ResultPair<ArrayList<JobApplicationCard>>> HELPER = new JobApplicationDAOTestHelper<>(ACTUAL, EXPECTED);
+
+        HELPER.verifyResultPairOfJobAppCardsAreEqual();
+        assertCallCountForGetJobAppCards(USERNAME, 1);
+    }
+
+    private void assertCallCountForGetJobAppCards(final String USERNAME, final int EXPECTED_COUNT) throws SQLException {
+        Mockito.verify(builder, Mockito.times(EXPECTED_COUNT)).getJobAppCards(USERNAME);
     }
 }
