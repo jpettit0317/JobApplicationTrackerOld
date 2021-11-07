b96a5f04559574c05172951818c15d33e62fde34
Jack Pettit <skelatorjack@gmail.com>
2021-11-07 15:49:49 -0600

    Imported things needed for controller testing.

--- backend/jobapplicationtrackerbackend/src/test/java/com/jpettit/jobapplicationtrackerbackend/controllers/UserControllerTest.java
+++ backend/jobapplicationtrackerbackend/src/test/java/com/jpettit/jobapplicationtrackerbackend/controllers/UserControllerTest.java
@@ -1,13 +1,27 @@
 package com.jpettit.jobapplicationtrackerbackend.controllers;
 
+import com.jpettit.jobapplicationtrackerbackend.enums.AppProperties;
+import com.jpettit.jobapplicationtrackerbackend.services.UserService;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = UserController.class)
+@SpringBootTest
+@SpringBootConfiguration
+@TestPropertySource(properties = {
+        "application.env=test",
+        "application.encryption.rounds=12"
+})
 class UserControllerTest {
 
 }
